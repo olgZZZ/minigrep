b30201b951f436625b041d8e45bb9b32a17faf8e
Holger <ollgert@gmail.com>
2022-08-08 21:52:30 +0300

    some commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Program "minigrep" from Rust book
+
+This chapter recapped some of the major concepts you’ve learned so far and covered how to perform common I/O operations in Rust. By using command line arguments, files, environment variables, and the  `eprintln!`  macro for printing errors.
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Parsing arguments problem: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Error in the application: {}", e);
+
+        process::exit(1);
+    }
+}
